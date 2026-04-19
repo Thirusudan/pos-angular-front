9ff950cb7aa6c87296f5fb80b3d6b15c2426de4c
Thirusudan <thirusudan110@gmail.com>
2026-04-19 12:09:00 +0530

    dashboard changes

--- src/app/pages/dashboard-page/dashboard-page.ts
+++ src/app/pages/dashboard-page/dashboard-page.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { DashboardMainHeader } from "./inner-pages/core/dashboard-main-header/dashboard-main-header";
 import { DashboardMainFooter } from "./inner-pages/core/dashboard-main-footer/dashboard-main-footer";
 import { TabBar } from '../../dto/TabBar';  
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [DashboardMainHeader, DashboardMainFooter,RouterLink, RouterOutlet],
+  imports: [DashboardMainHeader, DashboardMainFooter,RouterLink, RouterOutlet,RouterLinkActive],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.scss',
 })
